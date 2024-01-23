3bec2f64f82d1801b2b4dc530881f71b9e3d15e4
Remo3311 <remo33553311@gmail.com>
2024-01-23 15:38:19 -0800

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -1,30 +1,29 @@
-#ifndef _SORT_H
-#define _SORT_h
-
-#include <stdlib.h>
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
+
 /**
-* struct listint_s - Doubly linked list node
-*
-* @n: Integer stored in the node
-@ prev: Pointer to the previous element of the list
-@ next: Pointer to the next element of the list
-*/
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @next: Pointer to the next element of the list
+ * @prev: Pointer to the previous element of the list
+ */
 typedef struct listint_s
 {
-	int n;
+	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
-/* print */
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
+void quick_sort_really(int *array, size_t size, int *whole_array,
+		       size_t full_size);
+size_t partitioner(int *array, size_t size, int *whole_array, size_t);
 #endif
