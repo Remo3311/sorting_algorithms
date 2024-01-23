a274416be260ea0144962afa59f1986209d1fc39
Remo3311 <remo33553311@gmail.com>
2024-01-23 15:35:31 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,39 @@
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *bubble_sort - Function that sorts an array of ints
- *using bubble sort algorithm
- *
- *@array: array of ints
- *@size: size of array
- *
+ * bubble_sort - Sort by swapping largest items to end of array
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-int tem;
-size_t j, q;
+	size_t i;
+	int swap;
+	size_t tem;
+	size_t temsize;
 
-if (!array || !size)
-return;
-
-j = 0;
-while (j < size)
-{
-for (q = 0; q < size - 1; q++)
-{
-if (array[q] > array[q + 1])
-{
-tem = array[q];
-array[q] = array[q + 1];
-array[q + 1] = tem;
-print_array(array, size);
-}
-}
-j++;
+	if (array == NULL)
+		return;
+	temsize = size;
+	swap = 1;
+	while (swap)
+	{
+		swap = 0;
+		i = 0;
+		while (i < temsize - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tem = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tem;
+				print_array(array, size);
+				swap = 1;
+			}
+			i++;
+		}
+		temsize = i;
 }
 }
